2ca6f925e859a7fc4ab3e7b1deb39e83dac78971
ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFCpkaQgLdANfmSqQ65NlnJ1Q592NjLqG5iHhyENjG9HTPNUSdX8y+sjvKnb7h1B1P2s9jPnlVG3aXb22rK30MG0Y4dCLMwL20FMGc7+4jYH70S8OxetWUgxY7y9/5xdmAJ3AWvuuMNf9s0KMKIfYJl85V8kTAub6JsH/pSlCtT1U56Kt1ao0ipV/o6evBsDV/R1X9gfOspgx53DKuwl//goanRAtqXoEURFW/nt4L8DPfBodtMS+OBtIqR3SobFSd0bChP2PxUD5g08QfYyonX1MZHYWJqEq/T8N/K0N9nfuDjEBm62ELvrK4MWosJuwFLOx+UyzyjIRdsx6NSIf+KIOtWLtKx4ldUWzDhrJyN6m3jNT9JRArh1yO61nZfojLgZ6HKoUYSR85MeuZ4ms+sH7WZL7e7Ht5u/gQi5hBT0cvA4Opky8l4uo7tdi9RUMXZKYHUGIZRzUiQAFRjSMdJ8cUTZmhv6OtLDe/NKEa+m5rBrrdyrVJVPP/C8d/Tlk= wdbailey87@gmail.com <wdbailey87@gmail.com>
2021-11-06 14:09:29 -0700

    finish code along

--- server.js
+++ server.js
@@ -18,6 +18,45 @@ app.get('/about', (req, res) => {
   res.json({ message: 'this is a practice express app!' })
 })
 
+/**
+ * (http://)              (www.example.com)   (/users/:userId/comments/:commentId)          (?)                          search=recent   & otherQuery=value
+ * (the protocol to use)  (host/domain)       (request parameters -- path to the resource)   (starts the query strings)  (key=value pairs)
+ * https://www.hulu.com/watch/49af3f2f-6ced-4716-94fe-5002008651f2
+ * query strings are used on GET request (for instance a search term)
+ * https://www.google.com/search?q=mango
+ */
+
+// example query string for searching on a get request
+app.get('/search', (req, res) => {
+  console.log(req.query) // all query strings come in on the req.query (req.body) (req.headers)
+  res.json({ q: req.query.q })
+})
+
+// /:variableName -- is like creating a variable in express
+// 'compound route' with more that one variable -- /:variable1/:varaible2
+app.get('/watch/:id', (req, res) => {
+  console.log(req.params) // all of the request parameters
+  // look up the req.params.id in the datavase -- send information from the database to the client
+  res.json({ id: req.params.id })
+})
+
+// READ one comment by one user
+app.get('/users/:userId/comments/:commentId', (req, res) => {
+  console.log(req.params)
+  res.json({ msg: 'look up user id and comment id' })
+})
+
+//takes two route params /:x/:y and adds them together
+app.get('/calc/:x/:y', (req, res) => {
+  // take the route params x and y and add them (after turing the strings into numbers)
+  // /calc/5/10 -- response should be 15 
+  const x = parseInt(req.params.x)
+  const y = parseInt(req.params.y)
+  const sum = x + y
+  // send back the result of the calculation
+  res.json({ sum: sum })
+})
+
 // tell express what port to listen on for incoming requests
 app.listen(port, () => {
   console.log(`listening to the smooth sounds of port ${port} in the morning 🌊`)
